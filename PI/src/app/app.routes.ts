40c57f3b0b9e30dc62a68bea41e388ec25f9f90e
Josep Boronat <josepboronat@MacBook-Air-de-Josep.local>
2025-02-11 11:25:32 +0100

    routing para contacto

--- PI/src/app/app.routes.ts
+++ PI/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './views/inicio/inicio.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
-
-    {path: '', redirectTo: 'inicio', pathMatch: 'full'}
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+    {path: 'contacto', component: ContactoComponent},
+    {path: 'contacto', redirectTo: 'contacto', pathMatch: 'full'}
 ];
