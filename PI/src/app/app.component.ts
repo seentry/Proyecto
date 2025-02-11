30fdd887cf44a53542f9e6db45715d9665c28e59
cafebo <cafebo@alumnatflorida.es>
2025-02-11 12:16:27 +0100

    añadir vistas y modificar

--- PI/src/app/app.component.ts
+++ PI/src/app/app.component.ts
@@ -2,13 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { CardComponent } from './components/card/card.component';
-import { ContactoComponent } from './views/contacto/contacto.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CardComponent, ContactoComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- PI/src/app/app.routes.ts
+++ PI/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { InicioComponent } from './views/inicio/inicio.component';
 import { ContactoComponent } from './views/contacto/contacto.component';
+import { TiendaComponent } from './views/tienda/tienda.component';
+import { ReservaComponent } from './views/reserva/reserva.component';
+import { CentroComponent } from './views/centro/centro.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
-    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
+    {path: 'centro', component: CentroComponent},
+    {path: 'tienda', component: TiendaComponent},
+    {path: 'reserva', component: ReservaComponent},
     {path: 'contacto', component: ContactoComponent},
-    {path: 'contacto', redirectTo: 'contacto', pathMatch: 'full'}
+
+    {path: '', redirectTo: 'inicio', pathMatch: 'full'},
 ];
