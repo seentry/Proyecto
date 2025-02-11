17bcc1ad2e5cae92c554592790a73dbb0a78c960
Josep Boronat <josepboronat@MacBook-Air-de-Josep.local>
2025-02-11 10:39:39 +0100

    creacion contacto

--- PI/src/app/app.component.ts
+++ PI/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CardComponent } from './components/card/card.component';
+import { ContactoComponent } from './views/contacto/contacto.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CardComponent, ContactoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
