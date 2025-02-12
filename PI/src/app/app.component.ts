50ce685b1ce2e061d29c0509c055c68bf80953f8
jvboronatjosep <jvboronatdavid@gamil.com>
2025-02-12 12:57:14 +0100

    form iniciar session, registrarse

--- PI/src/app/app.component.ts
+++ PI/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { IniciarSesionComponent } from './views/iniciar-sesion/iniciar-sesion.component';
+import { RegistrarseComponent } from './views/registrarse/registrarse.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, IniciarSesionComponent, RegistrarseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
