d3b0d574cd7755ad7c99769508684194875be0d8
jvboronatjosep <jvboronatdavid@gamil.com>
2025-02-13 11:26:02 +0100

    nav para secundario y nav para la principal

--- PI/src/app/app.component.ts
+++ PI/src/app/app.component.ts
@@ -5,20 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IniciarSesionComponent } from './views/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './views/registrarse/registrarse.component';
-<<<<<<< HEAD
-import { ReservaComponent } from './views/reserva/reserva.component';
-=======
 import { HeaderInicioComponent } from './components/header-inicio/header-inicio.component';
->>>>>>> 737bb9ab92c7cb989e591249f499e2bb343838cb
 
 
 @Component({
   selector: 'app-root',
-<<<<<<< HEAD
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, IniciarSesionComponent, RegistrarseComponent, ReservaComponent],
-=======
   imports: [RouterOutlet, HeaderComponent, FooterComponent, IniciarSesionComponent, RegistrarseComponent, HeaderInicioComponent],
->>>>>>> 737bb9ab92c7cb989e591249f499e2bb343838cb
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
