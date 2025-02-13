b75161e51d8501a2914346800e0cbd81f7960ffc
jvboronatjosep <jvboronatdavid@gamil.com>
2025-02-13 09:47:03 +0100

    view reserva

--- PI/src/app/app.component.ts
+++ PI/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { IniciarSesionComponent } from './views/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './views/registrarse/registrarse.component';
+import { ReservaComponent } from './views/reserva/reserva.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, IniciarSesionComponent, RegistrarseComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, IniciarSesionComponent, RegistrarseComponent, ReservaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
