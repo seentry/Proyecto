3f67e1d6eb95a425deb8a37482028077ed4bd9d1
cafebo <cafebo@alumnatflorida.es>
2025-02-14 12:03:59 +0100

    empezando la vista del perfil

--- PI/src/app/app.routes.ts
+++ PI/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ReservaComponent } from './views/reserva/reserva.component';
 import { CentroComponent } from './views/centro/centro.component';
 import { IniciarSesionComponent } from './views/iniciar-sesion/iniciar-sesion.component';
 import { RegistrarseComponent } from './views/registrarse/registrarse.component';
+import { MiPerfilComponent } from './views/mi-perfil/mi-perfil.component';
 
 export const routes: Routes = [
     {path: 'inicio', component: InicioComponent},
@@ -15,6 +16,7 @@ export const routes: Routes = [
     {path: 'contacto', component: ContactoComponent},
     {path: 'inicio_sesion', component: IniciarSesionComponent},
     {path: 'registrarse', component: RegistrarseComponent},
+    {path: 'mi_perfil', component: MiPerfilComponent},
 
     {path: '', redirectTo: 'inicio', pathMatch: 'full'},
 ];
