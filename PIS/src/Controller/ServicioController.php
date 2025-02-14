3f657df45d639f4aaae726fbab7154cab1f101d6
Victor Gimenez <vigigi01@floridauniversitaria.es>
2025-02-14 10:16:43 +0100

    Creado controlador de servicios

---
+++ PIS/src/Controller/ServicioController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Servicio;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ServicioController extends AbstractController
+{
+    #[Route('/api/servicio', name: 'servicio', methods: 'GET', format: 'json')]
+    public function getAllServicios(EntityManagerInterface $entityManager): JsonResponse
+    {
+        $servicios = $entityManager->getRepository(Servicio::class)->findAll();
+        return $this->json($servicios, 200, [], ['groups' => ['servicio']]);
+    }
+
+    #[Route('/api/servicio', name: 'servicioCreate', methods: 'POST', format: 'json')]
+    public function createServicio(EntityManagerInterface $entityManager, Request $request): JsonResponse
+    {
+        $requestContent = json_decode($request->getContent(), true);
+        $servicio = new Servicio();
+
+        $servicio->setNombre($requestContent['nombre']);
+        $servicio->setDescripcion($requestContent['descripcion']);
+        $servicio->setStock($requestContent['stock']);
+        $servicio->setPrecio($requestContent['precio']);
+
+        $entityManager->persist($servicio);
+        $entityManager->flush();
+        return $this->json($servicio, 200, [], ['groups' => ['servicio']]);
+    }
+}
