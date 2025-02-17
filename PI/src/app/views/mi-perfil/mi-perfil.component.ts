86af173392a9a8bbec4521b6688ca7f193ca5388
dani <danielarastell302004@gmail.com>
2025-02-17 11:46:02 +0100

    Solucionando conflictos

--- PI/src/app/views/mi-perfil/mi-perfil.component.ts
+++ PI/src/app/views/mi-perfil/mi-perfil.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RequestService } from '../../services/request.service';
-import { Cliente } from '../../models/response.interfaceClientes';
-import { Cita } from '../../models/response.interfaceCita';
+import { Usuario } from '../../models/response.interface';
+import { Cita } from '../../models/response.interface';
 
 
 @Component({
@@ -10,43 +10,6 @@ import { Cita } from '../../models/response.interfaceCita';
   templateUrl: './mi-perfil.component.html',
   styleUrl: './mi-perfil.component.css'
 })
-export class MiPerfilComponent implements OnInit {
-  public userProfile: Cliente | null = null;
-  public userAppointments: Cita[] = [];
+export class MiPerfilComponent {
 
-  constructor(private service: RequestService) {}
-
-  ngOnInit(): void {
-    this.getUserProfile();
-    this.getUserAppointments();
-  }
-
-  public getUserProfile(): void {
-    this.service.getProfile().subscribe((response: Cliente) => {
-      console.log("Perfil recibido:", response);
-      this.userProfile = response;
-    }, (error) => {
-      console.error("Error al obtener perfil:", error);
-    });
-  }
-
-  public getUserAppointments(): void {
-    this.service.getAppointments().subscribe((response: Cita[]) => {
-      console.log("Citas recibidas:", response);
-      this.userAppointments = response;
-    }, (error) => {
-      console.error("Error al obtener citas:", error);
-    });
-  }
-
-  public cancelAppointment(id: number): void {
-    if (confirm("¿Estás seguro de que quieres cancelar esta cita?")) {
-      this.service.cancelAppointment(id).subscribe(() => {
-        this.userAppointments = this.userAppointments.filter(app => app.id !== id);
-        alert("Cita cancelada exitosamente");
-      }, (error) => {
-        console.error("Error al cancelar la cita:", error);
-      });
-    }
-  }
 }
--- PI/src/app/views/tienda/tienda.component.ts
+++ PI/src/app/views/tienda/tienda.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RequestService } from '../../services/request.service';
 import { Servicio } from '../../models/response.interface';
 import { CardComponent } from '../../components/card/card.component';
@@ -9,7 +9,7 @@ import { CardComponent } from '../../components/card/card.component';
   styleUrls: ['./tienda.component.css'],
   imports: [CardComponent]
 })
-export class TiendaComponent implements OnInit {
+export class TiendaComponent {
 
   constructor(private service: RequestService) { }
 
@@ -17,7 +17,7 @@ export class TiendaComponent implements OnInit {
 
   private apiUrlServicio: string = 'http://localhost:8000/api/servicio';
 
-  ngOnInit(): void {
+  ngOnInit(): void {  
     this.getServicios();
   }
 
@@ -31,56 +31,5 @@ export class TiendaComponent implements OnInit {
         console.error("Error al obtener servicios:", error);
       }
     );
-  }
-
-  public updateSearchTerm(event: Event): void {
-    this.searchTerm = (event.target as HTMLInputElement).value.trim();
-  }
-
-  public searchByButton(): void {
-    if (!this.searchTerm) {
-      this.filteredServicios = this.servicios;
-      return;
-    }
-
-    let idBusqueda = Number(this.searchTerm);
-    if (isNaN(idBusqueda)) {
-      alert("Por favor ingrese un ID válido.");
-      return;
-    }
-
-    let resultado = this.servicios.find(servicio => servicio.id === idBusqueda);
-    this.filteredServicios = resultado ? [resultado] : [];
-  }
-
-  public updateSortOrder(event: Event): void {
-    this.sortType = (event.target as HTMLSelectElement).value;
-    this.sortServicios();
-  }
-
-  public sortServicios(): void {
-    if (this.sortType === "id") {
-      this.filteredServicios = [...this.servicios].sort((a, b) => a.id - b.id);
-    } else if (this.sortType === "name") {
-      this.filteredServicios = [...this.servicios].sort((a, b) =>
-        a.cliente.nombre.localeCompare(b.cliente.nombre)
-      );
-    } else if (this.sortType === "dateSoon") {
-      this.filteredServicios = [...this.servicios].sort((a, b) => 
-        new Date(b.fecha).getTime() - new Date(a.fecha).getTime()
-      );
-    } else if (this.sortType === "dateLate") {
-      this.filteredServicios = [...this.servicios].sort((a, b) => 
-        new Date(a.fecha).getTime() - new Date(b.fecha).getTime()
-      );
-    } else if (this.sortType === "pay") {
-      this.filteredServicios = [...this.servicios].sort((a, b) => 
-        (b.pagado ? 1 : 0) - (a.pagado ? 1 : 0)
-      );
-    } else if (this.sortType === "noPay") {
-      this.filteredServicios = [...this.servicios].sort((a, b) => 
-        (a.pagado ? 1 : 0) - (b.pagado ? 1 : 0)
-      );
-    }
   }
 }
