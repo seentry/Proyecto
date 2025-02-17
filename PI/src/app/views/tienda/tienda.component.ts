3fddf0b219278b4e918ab1f514e543d4ef2ecd4e
dani <danielarastell302004@gmail.com>
2025-02-17 13:34:22 +0100

    Terminando pagina de Tienda

--- PI/src/app/views/tienda/tienda.component.ts
+++ PI/src/app/views/tienda/tienda.component.ts
@@ -13,8 +13,10 @@ export class TiendaComponent {
 
   constructor(private service: RequestService) { }
 
-  public servicios: Servicio[] = [];
-
+  public servicios: Servicio[] = []; 
+  public serviciosPaginados: Servicio[] = []; 
+  public currentPage: number = 1;  // Página actual
+  public itemsPerPage: number = 4; // Elementos por página
   private apiUrlServicio: string = 'http://localhost:8000/api/servicio';
 
   ngOnInit(): void {  
@@ -25,11 +27,37 @@ export class TiendaComponent {
     this.service.getServicios(this.apiUrlServicio).subscribe(
       (response) => {
         console.log("Servicios recibidos:", response);
-        this.servicios = response.filter(servicio => servicio.imagen !== null && servicio.imagen !== ''); //Filtra las imagenes que sean null, las elimina del array 
+        this.servicios = response.filter(servicio => servicio.imagen !== null && servicio.imagen !== ''); // Filtrar imágenes vacías
+        this.actualizarPaginacion(); 
       },
       (error) => {
         console.error("Error al obtener servicios:", error);
       }
     );
+  }
+
+  // Método para actualizar los datos paginados
+  private actualizarPaginacion(): void {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    this.serviciosPaginados = this.servicios.slice(startIndex, endIndex);
+  }
+
+  nextPage(): void {
+    if (this.currentPage < this.totalPages()) {
+      this.currentPage++;
+      this.actualizarPaginacion();
+    }
+  }
+
+  prevPage(): void {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.actualizarPaginacion();
+    }
+  }
+
+  totalPages(): number {
+    return Math.ceil(this.servicios.length / this.itemsPerPage);
   }
 }
