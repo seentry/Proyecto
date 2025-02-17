2904f6b58e45bed55416fcf0d5c7795b4b155657
jvboronatjosep <jvboronatdavid@gamil.com>
2025-02-17 20:54:06 +0100

    commit pagina principal me da error

--- PI/src/app/components/card-gestion-productos/card-gestion-productos.component.ts
+++ PI/src/app/components/card-gestion-productos/card-gestion-productos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-gestion-productos',
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class CardGestionProductosComponent {
 
+
+  @Input() servicio: string = "";
+  @Input() stock: number = 0;
+  @Input() precio: number = 0;
+  
 }
--- PI/src/app/views/inicio/inicio.component.ts
+++ PI/src/app/views/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { CardComponent } from '../../components/card/card.component';
 import { RequestService } from '../../services/request.service';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
 import { Servicio } from '../../models/response.interface';
-
+import { CardGestionProductosComponent } from '../../components/card-gestion-productos/card-gestion-productos.component';
 
 @Component({
   selector: 'app-inicio',
@@ -15,12 +15,21 @@ export class InicioComponent {
 
 
   public servicios: Servicio[] = [];
+
+  public servicio: string = "";
+  public stock: number = 0;
+  public precio: string = "";
+
+
   constructor(private service: RequestService) { }
 
   private apiUrlServicio: string = 'http://localhost:8000/api/servicio';
 
   public servicios_productos: string = "servicios";
   
+  public array_servicos: Servicio[] = [];
+  public array_productos: Servicio[] = [];
+
   public click_servicios(){
     console.log("servicios");
   }
@@ -34,11 +43,21 @@ export class InicioComponent {
   public getServicios(): void {
     this.service.getServicios(this.apiUrlServicio).subscribe((response) => {
       this.servicios = response;
-      console.log("Servicio: ", response);
+  
+      for (let index = 0; index < this.servicios.length; index++) {
+        if (this.servicios[index].stock == null) {  // Usamos this.servicios en lugar de response
+          this.array_servicos.push(this.servicios[index]);
+          //console.log("Servicio agregado a array_servicos:", this.array_servicos[this.array_servicos.length - 1]);
+        } else {
+          this.array_productos.push(this.servicios[index]);
+          //console.log("Producto agregado a array_productos:", this.array_productos[this.array_productos.length - 1]);
+        }
+      }
     }, (error) => {
       console.error("Error al obtener servicios:", error);
     });
   }
+  
 
   ngOnInit(): void {
     this.getServicios();
